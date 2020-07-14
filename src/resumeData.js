0a362b1b146d6d1499408ddbe3b3944db68b5129
Jérémie Guérinel <jguerinel@ifeelsmart.com>
2020-07-14 18:21:14 +0200

    Init commit.

---
+++ src/resumeData.js
@@ -0,0 +1,121 @@
+let resumeData = {
+    // "imagebaseurl":"http://localhost:3000/",
+    "imagebaseurl":"https://Moth13.github.io/",
+    "name": "Jeremie GUERINEL",
+    "role": "Backend Developer",
+    "linkedinId":"http://www.linkedin.com/in/jeremieguerinel",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"http://www.linkedin.com/in/jeremieguerinel",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Moth13",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"Backend UI engineer, such mix can be applied to me. I work at home since 2012, fully committed to my work.",
+    "address":"2 rue de la gare 35220 Chateaubourg",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"IFSIC",
+        "specialization":"Engineer degree, Digital imagery and communication",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2008",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"Rennes 1 University",
+        "specialization":"Deug STPI (Science and Technology for the Engineer)",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2004",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"iFeelsmart",
+        "specialization":"Backend engineer",
+        "MonthOfLeaving":"...",
+        "YearOfLeaving":"...",
+        "Achievements":"Some RestAPIs using go and/or python, deployment using kubernetees and helm on Google Cloud Platform, AWS,..."
+      },
+      {
+        "CompanyName":"iFeelsmart",
+        "specialization":"UI engineer",
+        "MonthOfLeaving":"August",
+        "YearOfLeaving":"2018",
+        "Achievements":"C++ UI for some setup-box android devices, some Recording or Recommandation engines, an internal AutoTest solution using Appium, Behave"
+      },
+      {
+        "CompanyName":"Altran",
+        "specialization":"UI engineer",
+        "MonthOfLeaving":"February",
+        "YearOfLeaving":"2012",
+        "Achievements":"C++ render engine and UI engine for a new generation of setup-box devices"
+      },
+      {
+        "CompanyName":"Artefacto",
+        "specialization":"3D engineer",
+        "MonthOfLeaving":"February",
+        "YearOfLeaving":"2009",
+        "Achievements":"Improvments of internal 3D live rendering (skin, water), tools for 3dsMax"
+      }
+    ],
+    "skillsDescription":"A glimps of my skills:",
+    "skills":[
+      {
+        "skillname":"C++",
+        "skillalias":"cpp"
+      },
+      {
+        "skillname":"Python",
+        "skillalias":"Python"
+      },
+      {
+        "skillname":"Go",
+        "skillalias":"go"
+      },
+      {
+        "skillname":"Reactjs",
+        "skillalias":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      // {
+      //   "name":"project1",
+      //   "description":"mobileapp",
+      //   "imgurl":"images/portfolio/phone.jpg"
+      // },
+      // {
+      //   "name":"project2",
+      //   "description":"mobileapp",
+      //   "imgurl":"images/portfolio/project.jpg"
+      // },
+      // {
+      //   "name":"project3",
+      //   "description":"mobileapp",
+      //   "imgurl":"images/portfolio/project2.png"
+      // },
+      // {
+      //   "name":"project4",
+      //   "description":"mobileapp",
+      //   "imgurl":"images/portfolio/phone.jpg"
+      // }
+    ],
+    "testimonials":[
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // },
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // }
+    ]
+    }
+  export default resumeData
